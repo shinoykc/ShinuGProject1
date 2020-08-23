71e8a7ca09fff5621654d7579eb301f16fd5abc1
Shinoykc <shinoykomath@gmail.com>
2020-08-23 11:50:18 +0530

    create Jframe

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,25 @@
+import javax.swing.JFrame;
+
 //create a class named Calculator
 public class Calculator {
 	
 	//create a constructor named Calculator
 	public Calculator(){
+		JFrame jf=new JFrame("My Calculator");// create object for JFrame
+		jf.setLayout(null);
+		jf.setSize(600,600);//define JFrame size
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );// for closing the window
 		
+		
+	//Write main method	
 	}
 	
+	public static void main(String []args) {
 	
+		//Calculator cal=new Calculator();// create object for Calculator class
+		new Calculator();
+		
+	}
 
 }
